a0bc5712f95dee7067ff58cbf78bd301f7d5110d
Lucy255 <lucyna.perdak@gmail.com>
2026-06-14 16:32:43 +0300

    Create 02day_calculator.py

---
+++ 02day/02day_calculator.py
@@ -0,0 +1,8 @@
+a = int(input("First number: "))
+b = int(input("Second number:  "))
+
+print(f"{a} + {b} = {a + b}")
+print(f"{a} - {b} = {a - b}")
+print(f"{a} / {b} = {a / b}")
+print(f"{a} * {b} = {a * b}")
+print(f"{a} ** {b} = {a ** b}")
